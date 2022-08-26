902cb7f47e2822ad08bbfe5a47a8f5103ecd0ab1
alevans99 <alevans99@gmail.com>
2022-08-26 16:51:26 +0100

    Adds timestamp to messages

--- src/components/pages/chat/sub-components/SingleMessage.vue
+++ src/components/pages/chat/sub-components/SingleMessage.vue
@@ -1,28 +1,38 @@
 <template >
   <div
-    :class="`single-message-container white my-2 my-md-4 pa-3 align-self-${messageAlignment(
+    :class="`single-message-container my-2 my-md-4 pa-3 align-self-${messageAlignment(
       messageObject.senderId
     )}`"
   >
     <h5
       :class="`text-body-2 text-md-body-1 text-md-center text-${messageAlignment(
         messageObject.senderId
-      )}`"
+      )} font-weight-bold`"
       style="word-break: break-word"
     >
       {{ messageObject.text }}
     </h5>
-    <p
-      :class="`mb-0 text-subtitle-2 text-md-subtitle-1 text-${messageAlignment(
+    <div
+      :class="`d-flex flex-column align-${messageAlignment(
         messageObject.senderId
-      )}`"
+      )} flex-wrap`"
     >
-      {{ messageObject.senderName }}
-    </p>
+      <p
+        :class="`mb-0 text-subtitle-2 text-md-subtitle-1 text-${messageAlignment(
+          messageObject.senderId
+        )} `"
+      >
+        {{ messageObject.senderName }}
+      </p>
+      <p :class="`mb-0 text-subtitle-2 text-md-subtitle-1 `">
+        {{ formatTimestamp(messageObject.timestamp) }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import { DateTime } from 'luxon'
 import { mapState } from 'vuex'
 export default {
   name: 'single-message',
@@ -38,6 +48,9 @@ export default {
     messageAlignment(senderId) {
       return senderId === this.clientId ? 'end' : 'start'
     },
+    formatTimestamp(isoDate) {
+      return DateTime.fromISO(isoDate).toLocaleString(DateTime.TIME_SIMPLE)
+    },
   },
 }
 </script>
@@ -46,6 +59,8 @@ export default {
   border-radius: 16px;
   min-width: 30%;
   max-width: 50%;
+  border: 2px black solid;
+  background-color: white;
 }
 
 @media only screen and (max-width: 500px) {
